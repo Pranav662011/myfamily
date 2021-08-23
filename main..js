2b77cfc97655fb2565e4c4f1cede574bd720a929
Pranav662011 <79525414+Pranav662011@users.noreply.github.com>
2021-08-23 12:13:23 +0530

    Add files via upload

---
+++ main..js
@@ -0,0 +1,20 @@
+var images=["father.jpg","mother.jpg","me..jpg","sis.jpg","grandmotheer.jpg","grandfathher"];
+
+var names=["Vijay Anand", "buvana", "pranav","akshara","rani","raja"];
+
+var i = 0
+
+function update(){
+
+    i++;
+    var member_of_family_array= 5
+    if(i > member_of_family_array)
+
+    {
+        i = 0
+       
+    }
+    var updateimage = image[i] 
+    var updatenames = names[i]
+    document.getElementById(famil)
+}
